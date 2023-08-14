0216e5229dab6565f7d255dc45dbace46a5e14d7
maddi manikanta <maddhimanikanta7@gmail.com>
2023-08-14 20:23:37 +0530

    first commit

---
+++ logical.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int age = 25;
+
+    if (age <= 18 || age >= 60) {
+        
+        printf("You are eligible, age: %d\n", age);
+    }
+
+
+    return 0;
+}
+
